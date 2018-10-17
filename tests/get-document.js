a9102743bb14ed332b0ff85403d5299f7fe3ba8d
Evan Prodromou <evan@prodromou.name>
2018-10-17 15:10:37 -0400

    tests/get-document uses supertest

--- tests/get-document.js
+++ tests/get-document.js
@@ -1,38 +1,29 @@
 // test route to GET a document
 
 const tap = require('tap')
-const mocks = require('node-mocks-http')
+const request = require('supertest')
+
+// XXX: here to keep the app from bouncing to https
+
+process.env.NODE_ENV = 'development'
 
 const main = async () => {
-  let router
-  await tap.test('Document router exists', async () => {
-    router = require('../routes/document')
+  let app
+  await tap.test('App exists', async () => {
+    app = require('../server').app
   })
-  await tap.type(router, 'function')
+  await tap.type(app, 'function')
 
-  const req = mocks.createRequest({
-    method: 'GET',
-    url: '/foo/publication/1/document/1',
-    params: {
-      nickname: 'foo',
-      pubid: '1',
-      docid: '1'
-    },
-    headers: {
-      Host: 'reader-api.test'
-    }
-  })
-
-  const res = mocks.createResponse({ req: req })
-
-  router(req, res)
+  const res = await request(app)
+    .get('/foo/publication/1/document/1')
+    .set('Host', 'reader-api.test')
 
   await tap.equal(
-    res.getHeader('Content-Type'),
+    res.get('Content-Type'),
     'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
   )
 
-  const body = JSON.parse(res._getData())
+  const body = res.body
 
   await tap.type(body, 'object')
 
