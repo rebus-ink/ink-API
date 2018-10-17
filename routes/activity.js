ec8a33589111253e1e1d07e04c513ec3b134e67c
Evan Prodromou <evan@prodromou.name>
2018-10-17 17:18:21 -0400

    Add authentication to activity endpoint

--- routes/activity.js
+++ routes/activity.js
@@ -1,70 +1,80 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
-router.get('/:nickname/activity/:actid', function (req, res, next) {
-  const nickname = req.params.nickname
-  const actid = req.params.actid
-  const host = req.headers.host
+router.get(
+  '/:nickname/activity/:actid',
+  passport.authenticate('jwt', { session: false }),
+  function (req, res, next) {
+    const nickname = req.params.nickname
+    const actid = req.params.actid
+    const host = req.headers.host
 
-  res.setHeader(
-    'Content-Type',
-    'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
-  )
+    if (req.user !== nickname) {
+      res.status(403).send(`Access to activity ${actid} disallowed`)
+      return
+    }
 
-  let act = null
+    res.setHeader(
+      'Content-Type',
+      'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+    )
 
-  if (actid.match(/create/)) {
-    act = {
-      '@context': [
-        'https://www.w3.org/ns/activitystreams',
-        { reader: 'https://rebus.foundation/ns/reader' }
-      ],
-      summaryMap: {
-        en: `${nickname} created 'Publication 1'`
-      },
-      type: 'Create',
-      id: `https://${host}/${nickname}/activity/${actid}`,
-      actor: {
-        id: `https://${host}/${nickname}/`,
-        type: 'Person',
+    let act = null
+
+    if (actid.match(/create/)) {
+      act = {
+        '@context': [
+          'https://www.w3.org/ns/activitystreams',
+          { reader: 'https://rebus.foundation/ns/reader' }
+        ],
         summaryMap: {
-          en: `User with nickname ${nickname}`
+          en: `${nickname} created 'Publication 1'`
+        },
+        type: 'Create',
+        id: `https://${host}/${nickname}/activity/${actid}`,
+        actor: {
+          id: `https://${host}/${nickname}/`,
+          type: 'Person',
+          summaryMap: {
+            en: `User with nickname ${nickname}`
+          }
+        },
+        object: {
+          type: 'reader:Publication',
+          id: `https://${host}/${nickname}/publication/1`,
+          name: `Publication 1`,
+          totalItems: 4
         }
-      },
-      object: {
-        type: 'reader:Publication',
-        id: `https://${host}/${nickname}/publication/1`,
-        name: `Publication 1`,
-        totalItems: 4
+      }
+    } else {
+      act = {
+        '@context': [
+          'https://www.w3.org/ns/activitystreams',
+          { reader: 'https://rebus.foundation/ns/reader' }
+        ],
+        summaryMap: {
+          en: `${nickname} `
+        },
+        type: 'Read',
+        id: `https://${host}/${nickname}/activity/${actid}`,
+        actor: {
+          id: `https://${host}/${nickname}/`,
+          type: 'Person',
+          summaryMap: {
+            en: `User with nickname ${nickname}`
+          }
+        },
+        object: {
+          type: 'Document',
+          id: `https://${host}/${nickname}/publication/1/document/1`,
+          name: `Publication 1 Document 1`
+        }
       }
     }
-  } else {
-    act = {
-      '@context': [
-        'https://www.w3.org/ns/activitystreams',
-        { reader: 'https://rebus.foundation/ns/reader' }
-      ],
-      summaryMap: {
-        en: `${nickname} `
-      },
-      type: 'Read',
-      id: `https://${host}/${nickname}/activity/${actid}`,
-      actor: {
-        id: `https://${host}/${nickname}/`,
-        type: 'Person',
-        summaryMap: {
-          en: `User with nickname ${nickname}`
-        }
-      },
-      object: {
-        type: 'Document',
-        id: `https://${host}/${nickname}/publication/1/document/1`,
-        name: `Publication 1 Document 1`
-      }
-    }
+
+    res.end(JSON.stringify(act))
   }
-
-  res.end(JSON.stringify(act))
-})
+)
 
 module.exports = router
--- tests/get-activity.js
+++ tests/get-activity.js
@@ -2,47 +2,98 @@
 
 const tap = require('tap')
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
 
 // XXX: here to keep the app from bouncing to https
 
 process.env.NODE_ENV = 'development'
 
 const main = async () => {
-  let app
+  await tap.test('Environment variables are set', async () => {
+    await tap.type(process.env.ISSUER, 'string')
+    await tap.type(process.env.SECRETORKEY, 'string')
+  })
+
+  let app = null
   await tap.test('App exists', async () => {
     app = require('../server').app
+    await tap.type(app, 'function')
   })
-  await tap.type(app, 'function')
 
-  const res = await request(app)
-    .get('/foo/activity/1create')
-    .set('Host', 'reader-api.test')
+  await tap.test(
+    'GET /<userid>/activity/<actid> with no authentication',
+    async () => {
+      const res = await request(app)
+        .get('/foo/activity/1create')
+        .set('Host', 'reader-api.test')
 
-  await tap.equal(res.statusCode, 200)
-
-  await tap.equal(
-    res.get('Content-Type'),
-    'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      await tap.equal(res.statusCode, 401)
+    }
   )
 
-  const body = res.body
+  await tap.test(
+    "GET /<userid>/activity/<actid> with other user's authentication",
+    async () => {
+      const options = {
+        subject: 'bar',
+        expiresIn: '24h',
+        issuer: process.env.ISSUER
+      }
 
-  await tap.type(body, 'object')
+      const token = jwt.sign({}, process.env.SECRETORKEY, options)
 
-  await tap.type(body['@context'], 'object')
-  await tap.ok(Array.isArray(body['@context']))
+      const res = await request(app)
+        .get('/foo/activity/1create')
+        .set('Host', 'reader-api.test')
+        .set('Authorization', `Bearer ${token}`)
 
-  await tap.equal(body.id, 'https://reader-api.test/foo/activity/1create')
-  await tap.equal(body.type, 'Create')
-  await tap.type(body.summaryMap, 'object')
-  await tap.type(body.summaryMap.en, 'string')
-  await tap.type(body.actor, 'object')
-  await tap.type(body.actor.id, 'string')
-  await tap.type(body.actor.summaryMap, 'object')
-  await tap.type(body.actor.summaryMap.en, 'string')
-  await tap.type(body.object, 'object')
-  await tap.type(body.object.id, 'string')
-  await tap.type(body.object.name, 'string')
+      await tap.equal(res.statusCode, 403)
+    }
+  )
+
+  await tap.test(
+    "GET /<userid>/activity/<actid> with user's authentication",
+    async () => {
+      const options = {
+        subject: 'foo',
+        expiresIn: '24h',
+        issuer: process.env.ISSUER
+      }
+
+      const token = jwt.sign({}, process.env.SECRETORKEY, options)
+
+      const res = await request(app)
+        .get('/foo/activity/1create')
+        .set('Host', 'reader-api.test')
+        .set('Authorization', `Bearer ${token}`)
+
+      await tap.equal(res.statusCode, 200)
+
+      await tap.equal(
+        res.get('Content-Type'),
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      )
+
+      const body = res.body
+
+      await tap.type(body, 'object')
+
+      await tap.type(body['@context'], 'object')
+      await tap.ok(Array.isArray(body['@context']))
+
+      await tap.equal(body.id, 'https://reader-api.test/foo/activity/1create')
+      await tap.equal(body.type, 'Create')
+      await tap.type(body.summaryMap, 'object')
+      await tap.type(body.summaryMap.en, 'string')
+      await tap.type(body.actor, 'object')
+      await tap.type(body.actor.id, 'string')
+      await tap.type(body.actor.summaryMap, 'object')
+      await tap.type(body.actor.summaryMap.en, 'string')
+      await tap.type(body.object, 'object')
+      await tap.type(body.object.id, 'string')
+      await tap.type(body.object.name, 'string')
+    }
+  )
 }
 
 main()
