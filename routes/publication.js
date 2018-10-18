f2510d5b78900073c35ff516301e1ecf40b7c0b3
Evan Prodromou <evan@prodromou.name>
2018-10-18 13:42:03 -0400

    Add authentication to publication endpoint

--- routes/publication.js
+++ routes/publication.js
@@ -1,54 +1,64 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
-router.get('/:nickname/publication/:pubid', function (req, res, next) {
-  const nickname = req.params.nickname
-  const pubid = req.params.pubid
-  const host = req.headers.host
+router.get(
+  '/:nickname/publication/:pubid',
+  passport.authenticate('jwt', { session: false }),
+  function (req, res, next) {
+    const nickname = req.params.nickname
+    const pubid = req.params.pubid
+    const host = req.headers.host
 
-  res.setHeader(
-    'Content-Type',
-    'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
-  )
+    if (req.user !== nickname) {
+      res.status(403).send(`Access to document ${pubid} disallowed`)
+      return
+    }
 
-  res.end(
-    JSON.stringify({
-      '@context': [
-        'https://www.w3.org/ns/activitystreams',
-        { reader: 'https://rebus.foundation/ns/reader' }
-      ],
-      type: 'reader:Publication',
-      id: `https://${host}/${nickname}/publication/${pubid}`,
-      name: `Publication ${pubid}`,
-      attributedTo: {
-        type: 'Person',
-        name: 'Sample Author'
-      },
-      totalItems: 4,
-      orderedItems: [
-        {
-          type: 'Document',
-          id: `https://${host}/${nickname}/publication/${pubid}/document/1`,
-          name: 'Chapter 1'
+    res.setHeader(
+      'Content-Type',
+      'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+    )
+
+    res.end(
+      JSON.stringify({
+        '@context': [
+          'https://www.w3.org/ns/activitystreams',
+          { reader: 'https://rebus.foundation/ns/reader' }
+        ],
+        type: 'reader:Publication',
+        id: `https://${host}/${nickname}/publication/${pubid}`,
+        name: `Publication ${pubid}`,
+        attributedTo: {
+          type: 'Person',
+          name: 'Sample Author'
         },
-        {
-          type: 'Document',
-          id: `https://${host}/${nickname}/publication/${pubid}/document/2`,
-          name: 'Chapter 2'
-        },
-        {
-          type: 'Document',
-          id: `https://${host}/${nickname}/publication/${pubid}/document/3`,
-          name: 'Chapter 3'
-        },
-        {
-          type: 'Document',
-          id: `https://${host}/${nickname}/publication/${pubid}/document/4`,
-          name: 'Chapter 4'
-        }
-      ]
-    })
-  )
-})
+        totalItems: 4,
+        orderedItems: [
+          {
+            type: 'Document',
+            id: `https://${host}/${nickname}/publication/${pubid}/document/1`,
+            name: 'Chapter 1'
+          },
+          {
+            type: 'Document',
+            id: `https://${host}/${nickname}/publication/${pubid}/document/2`,
+            name: 'Chapter 2'
+          },
+          {
+            type: 'Document',
+            id: `https://${host}/${nickname}/publication/${pubid}/document/3`,
+            name: 'Chapter 3'
+          },
+          {
+            type: 'Document',
+            id: `https://${host}/${nickname}/publication/${pubid}/document/4`,
+            name: 'Chapter 4'
+          }
+        ]
+      })
+    )
+  }
+)
 
 module.exports = router
--- tests/get-publication.js
+++ tests/get-publication.js
@@ -1,60 +1,114 @@
-// test route to GET a user
+// test route to GET a publication
 
 const tap = require('tap')
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
 
 // XXX: here to keep the app from bouncing to https
 
 process.env.NODE_ENV = 'development'
 
 const main = async () => {
-  let app
+  await tap.test('Environment variables are set', async () => {
+    await tap.type(process.env.ISSUER, 'string')
+    await tap.type(process.env.SECRETORKEY, 'string')
+  })
+
+  let app = null
   await tap.test('App exists', async () => {
     app = require('../server').app
+    await tap.type(app, 'function')
   })
-  await tap.type(app, 'function')
 
-  const res = await request(app)
-    .get('/foo/publication/1')
-    .set('Host', 'reader-api.test')
+  await tap.test(
+    'GET /<userid>/publication/<pubid> with no authentication',
+    async () => {
+      const res = await request(app)
+        .get('/foo/publication/1')
+        .set('Host', 'reader-api.test')
 
-  await tap.equal(res.statusCode, 200)
-
-  await tap.equal(
-    res.get('Content-Type'),
-    'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      await tap.equal(res.statusCode, 401)
+    }
   )
 
-  const body = res.body
+  await tap.test(
+    'GET /<userid>/publication/<pubid> as other user',
+    async () => {
+      const options = {
+        subject: 'bar',
+        expiresIn: '24h',
+        issuer: process.env.ISSUER
+      }
 
-  await tap.type(body, 'object')
+      const token = jwt.sign({}, process.env.SECRETORKEY, options)
 
-  await tap.ok(Array.isArray(body['@context']))
-  await tap.equal(body['@context'].length, 2)
-  await tap.equal(body['@context'][0], 'https://www.w3.org/ns/activitystreams')
-  await tap.type(body['@context'][1], 'object')
-  await tap.equal(
-    body['@context'][1].reader,
-    'https://rebus.foundation/ns/reader'
+      const res = await request(app)
+        .get('/foo/publication/1')
+        .set('Host', 'reader-api.test')
+        .set('Authorization', `Bearer ${token}`)
+
+      await tap.equal(res.statusCode, 403)
+    }
   )
 
-  await tap.equal(body.id, 'https://reader-api.test/foo/publication/1')
-  await tap.equal(body.type, 'reader:Publication')
-  await tap.ok(body.name)
-  await tap.ok(body.attributedTo)
-  await tap.ok(Array.isArray(body.orderedItems))
-  await tap.ok(body.orderedItems.length > 0)
+  await tap.test(
+    'GET /<userid>/publication/<pubid> with correct authentication',
+    async () => {
+      const options = {
+        subject: 'foo',
+        expiresIn: '24h',
+        issuer: process.env.ISSUER
+      }
 
-  for (let i = 0; i < body.orderedItems.length; i++) {
-    let item = body.orderedItems[i]
-    await tap.type(item, 'object')
-    await tap.match(
-      item.id,
-      /^https:\/\/reader-api.test\/foo\/publication\/1\/document/
-    )
-    await tap.equal(item.type, 'Document')
-    await tap.ok(item.name)
-  }
+      const token = jwt.sign({}, process.env.SECRETORKEY, options)
+
+      const res = await request(app)
+        .get('/foo/publication/1')
+        .set('Host', 'reader-api.test')
+        .set('Authorization', `Bearer ${token}`)
+
+      await tap.equal(res.statusCode, 200)
+
+      await tap.equal(
+        res.get('Content-Type'),
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      )
+
+      const body = res.body
+
+      await tap.type(body, 'object')
+
+      await tap.ok(Array.isArray(body['@context']))
+      await tap.equal(body['@context'].length, 2)
+      await tap.equal(
+        body['@context'][0],
+        'https://www.w3.org/ns/activitystreams'
+      )
+      await tap.type(body['@context'][1], 'object')
+      await tap.equal(
+        body['@context'][1].reader,
+        'https://rebus.foundation/ns/reader'
+      )
+
+      await tap.equal(body.id, 'https://reader-api.test/foo/publication/1')
+      await tap.equal(body.type, 'reader:Publication')
+      await tap.ok(body.name)
+      await tap.ok(body.attributedTo)
+      await tap.ok(Array.isArray(body.orderedItems))
+      await tap.ok(body.orderedItems.length > 0)
+
+      for (let i = 0; i < body.orderedItems.length; i++) {
+        let item = body.orderedItems[i]
+        await tap.type(item, 'object')
+        await tap.match(
+          item.id,
+          /^https:\/\/reader-api.test\/foo\/publication\/1\/document/
+        )
+        await tap.equal(item.type, 'Document')
+        await tap.ok(item.name)
+      }
+    }
+  )
 }
 
 main()
