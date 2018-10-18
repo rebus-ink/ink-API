9094faa1306406e310583233863155a3c5ccaa43
Evan Prodromou <evan@prodromou.name>
2018-10-18 14:10:42 -0400

    Add authentication to library endpoint

--- routes/user-library.js
+++ routes/user-library.js
@@ -1,83 +1,93 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
-router.get('/:nickname/library', function (req, res, next) {
-  const nickname = req.params.nickname
-  const host = req.headers.host
+router.get(
+  '/:nickname/library',
+  passport.authenticate('jwt', { session: false }),
+  function (req, res, next) {
+    const nickname = req.params.nickname
+    const host = req.headers.host
 
-  res.setHeader(
-    'Content-Type',
-    'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
-  )
+    if (req.user !== nickname) {
+      res.status(403).send(`Access to library for ${nickname} disallowed`)
+      return
+    }
 
-  res.end(
-    JSON.stringify({
-      '@context': [
-        'https://www.w3.org/ns/activitystreams',
-        { reader: 'https://rebus.foundation/ns/reader' }
-      ],
-      name: `${nickname} library`,
-      type: 'Collection',
-      id: `https://${host}/${nickname}/library`,
-      totalItems: 6,
-      items: [
-        {
-          type: 'reader:Publication',
-          id: `https://${host}/${nickname}/publication/1`,
-          name: 'Publication 1',
-          attributedTo: {
-            type: 'Person',
-            name: 'Sample Author 1'
+    res.setHeader(
+      'Content-Type',
+      'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+    )
+
+    res.end(
+      JSON.stringify({
+        '@context': [
+          'https://www.w3.org/ns/activitystreams',
+          { reader: 'https://rebus.foundation/ns/reader' }
+        ],
+        name: `${nickname} library`,
+        type: 'Collection',
+        id: `https://${host}/${nickname}/library`,
+        totalItems: 6,
+        items: [
+          {
+            type: 'reader:Publication',
+            id: `https://${host}/${nickname}/publication/1`,
+            name: 'Publication 1',
+            attributedTo: {
+              type: 'Person',
+              name: 'Sample Author 1'
+            }
+          },
+          {
+            type: 'reader:Publication',
+            id: `https://${host}/${nickname}/publication/2`,
+            name: 'Publication 2',
+            attributedTo: {
+              type: 'Person',
+              name: 'Sample Author 2'
+            }
+          },
+          {
+            type: 'reader:Publication',
+            id: `https://${host}/${nickname}/publication/3`,
+            name: 'Publication 3',
+            attributedTo: {
+              type: 'Person',
+              name: 'Sample Author 3'
+            }
+          },
+          {
+            type: 'reader:Publication',
+            id: `https://${host}/${nickname}/publication/4`,
+            name: 'Publication 4',
+            attributedTo: {
+              type: 'Person',
+              name: 'Sample Author 4'
+            }
+          },
+          {
+            type: 'reader:Publication',
+            id: `https://${host}/${nickname}/publication/5`,
+            name: 'Publication 5',
+            attributedTo: {
+              type: 'Person',
+              name: 'Sample Author 5'
+            }
+          },
+          {
+            type: 'reader:Publication',
+            id: `https://${host}/${nickname}/publication/6`,
+            name: 'Publication 6',
+            attributedTo: {
+              type: 'Person',
+              name: 'Sample Author 6'
+            }
           }
-        },
-        {
-          type: 'reader:Publication',
-          id: `https://${host}/${nickname}/publication/2`,
-          name: 'Publication 2',
-          attributedTo: {
-            type: 'Person',
-            name: 'Sample Author 2'
-          }
-        },
-        {
-          type: 'reader:Publication',
-          id: `https://${host}/${nickname}/publication/3`,
-          name: 'Publication 3',
-          attributedTo: {
-            type: 'Person',
-            name: 'Sample Author 3'
-          }
-        },
-        {
-          type: 'reader:Publication',
-          id: `https://${host}/${nickname}/publication/4`,
-          name: 'Publication 4',
-          attributedTo: {
-            type: 'Person',
-            name: 'Sample Author 4'
-          }
-        },
-        {
-          type: 'reader:Publication',
-          id: `https://${host}/${nickname}/publication/5`,
-          name: 'Publication 5',
-          attributedTo: {
-            type: 'Person',
-            name: 'Sample Author 5'
-          }
-        },
-        {
-          type: 'reader:Publication',
-          id: `https://${host}/${nickname}/publication/6`,
-          name: 'Publication 6',
-          attributedTo: {
-            type: 'Person',
-            name: 'Sample Author 6'
-          }
-        }
-      ]
-    })
-  )
-})
+        ]
+      })
+    )
+  }
+)
 
 module.exports = router
--- tests/get-user-library.js
+++ tests/get-user-library.js
@@ -1,47 +1,92 @@
-// test route to GET a user
+// test route to GET a user library
 
 const tap = require('tap')
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+
 // XXX: here to keep the app from bouncing to https
 
 process.env.NODE_ENV = 'development'
 
 const main = async () => {
-  let app
+  await tap.test('Environment variables are set', async () => {
+    await tap.type(process.env.ISSUER, 'string')
+    await tap.type(process.env.SECRETORKEY, 'string')
+  })
+
+  let app = null
   await tap.test('App exists', async () => {
     app = require('../server').app
+    await tap.type(app, 'function')
   })
-  await tap.type(app, 'function')
 
-  const res = await request(app)
-    .get('/foo/library')
-    .set('Host', 'reader-api.test')
+  await tap.test('GET /<userid>/library with no authentication', async () => {
+    const res = await request(app)
+      .get('/foo/library')
+      .set('Host', 'reader-api.test')
 
-  await tap.equal(res.statusCode, 200)
+    await tap.equal(res.statusCode, 401)
+  })
 
-  await tap.equal(
-    res.get('Content-Type'),
-    'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+  await tap.test('GET /<userid>/library as other user', async () => {
+    const options = {
+      subject: 'bar',
+      expiresIn: '24h',
+      issuer: process.env.ISSUER
+    }
+
+    const token = jwt.sign({}, process.env.SECRETORKEY, options)
+
+    const res = await request(app)
+      .get('/foo/library')
+      .set('Host', 'reader-api.test')
+      .set('Authorization', `Bearer ${token}`)
+
+    await tap.equal(res.statusCode, 403)
+  })
+
+  await tap.test(
+    'GET /<userid>/library with correct authentication',
+    async () => {
+      const options = {
+        subject: 'foo',
+        expiresIn: '24h',
+        issuer: process.env.ISSUER
+      }
+
+      const token = jwt.sign({}, process.env.SECRETORKEY, options)
+
+      const res = await request(app)
+        .get('/foo/library')
+        .set('Host', 'reader-api.test')
+        .set('Authorization', `Bearer ${token}`)
+      await tap.equal(res.statusCode, 200)
+
+      await tap.equal(
+        res.get('Content-Type'),
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      )
+
+      const body = res.body
+
+      await tap.type(body, 'object')
+
+      await tap.ok(body['@context'])
+      await tap.equal(body.id, 'https://reader-api.test/foo/library')
+      await tap.equal(body.type, 'Collection')
+      await tap.ok(Array.isArray(body.items))
+      await tap.ok(body.items.length > 0)
+
+      for (let i = 0; i < body.items.length; i++) {
+        let item = body.items[i]
+        await tap.type(item, 'object')
+        await tap.match(item.id, /^https:\/\/reader-api.test\/foo\/publication/)
+        await tap.equal(item.type, 'reader:Publication')
+        await tap.ok(item.name)
+        await tap.ok(item.attributedTo)
+      }
+    }
   )
-
-  const body = res.body
-
-  await tap.type(body, 'object')
-
-  await tap.ok(body['@context'])
-  await tap.equal(body.id, 'https://reader-api.test/foo/library')
-  await tap.equal(body.type, 'Collection')
-  await tap.ok(Array.isArray(body.items))
-  await tap.ok(body.items.length > 0)
-
-  for (let i = 0; i < body.items.length; i++) {
-    let item = body.items[i]
-    await tap.type(item, 'object')
-    await tap.match(item.id, /^https:\/\/reader-api.test\/foo\/publication/)
-    await tap.equal(item.type, 'reader:Publication')
-    await tap.ok(item.name)
-    await tap.ok(item.attributedTo)
-  }
 }
 
 main()
