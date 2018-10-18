d02f6057ec2ea94232a13675166ad874bd5617bf
Evan Prodromou <evan@prodromou.name>
2018-10-18 14:17:33 -0400

    Add authentication to streams endpoint

--- routes/user-streams.js
+++ routes/user-streams.js
@@ -1,31 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
-router.get('/:nickname/streams', function (req, res, next) {
-  const nickname = req.params.nickname
-  const host = req.headers.host
+router.get(
+  '/:nickname/streams',
+  passport.authenticate('jwt', { session: false }),
+  function (req, res, next) {
+    const nickname = req.params.nickname
+    const host = req.headers.host
 
-  res.setHeader(
-    'Content-Type',
-    'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
-  )
+    if (req.user !== nickname) {
+      res.status(403).end(`Access to streams for ${nickname} disallowed`)
+      return
+    }
 
-  res.end(
-    JSON.stringify({
-      '@context': 'https://www.w3.org/ns/activitystreams',
-      name: `${nickname} streams`,
-      type: 'Collection',
-      id: `https://${host}/${nickname}/streams`,
-      totalItems: 1,
-      items: [
-        {
-          type: 'Collection',
-          id: `https://${host}/${nickname}/library`,
-          name: `${nickname}'s library`
-        }
-      ]
-    })
-  )
-})
+    res.setHeader(
+      'Content-Type',
+      'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+    )
+
+    res.end(
+      JSON.stringify({
+        '@context': 'https://www.w3.org/ns/activitystreams',
+        name: `${nickname} streams`,
+        type: 'Collection',
+        id: `https://${host}/${nickname}/streams`,
+        totalItems: 1,
+        items: [
+          {
+            type: 'Collection',
+            id: `https://${host}/${nickname}/library`,
+            name: `${nickname}'s library`
+          }
+        ]
+      })
+    )
+  }
+)
 
 module.exports = router
