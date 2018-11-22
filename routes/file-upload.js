cd885fa201be2aaeb51b774e53c431afa16eab6f
Marie <marie.pelletier@gmail.com>
2018-11-22 14:44:47 -0500

    added missing imports

--- routes/file-upload.js
+++ routes/file-upload.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const { Storage } = require('@google-cloud/storage')
+const passport = require('passport')
+const { getId } = require('../utils/get-id.js')
 
 const storage = new Storage()
 
