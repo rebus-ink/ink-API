70fc3e8c05736f43e7736fe74ce3bc84e6a2151e
Marie <marie.pelletier@gmail.com>
2018-12-19 09:29:30 -0500

    test for get outbox route

--- tests/unit/get-outbox-route.test.js
+++ tests/unit/get-outbox-route.test.js
@@ -7,6 +7,7 @@ const passport = require('passport')
 const { ExtractJwt } = require('passport-jwt')
 const MockStrategy = require('passport-mock-strategy')
 const { Reader } = require('../../models/Reader')
+const { Activity } = require('../../models/Activity')
 
 const setupPassport = () => {
   var opts = {}
@@ -20,13 +21,40 @@ const setupPassport = () => {
 
 setupPassport()
 
-const reader = Object.assign(new Reader(), {
-  id: '0dad66d5-670f-41e1-886a-b2e25b510b2d',
-  json: { name: 'J. Random Reader', userId: 'auth0|foo1545149868964' },
-  userId: 'auth0|foo1545149868964',
-  published: '2018-12-18T16:17:49.077Z',
-  updated: '2018-12-18 16:17:49'
+const activity = Object.assign(new Activity(), {
+  id: 'b902f2a9-54e0-4fa3-a241-05321fa77c93',
+  type: 'Activity',
+  json: {
+    '@context': 'https://www.w3.org/ns/activitystreams',
+    type: 'Arrive',
+    location: {
+      id: 'https://places.test/rebus-foundation-office',
+      type: 'Place',
+      nameMap: { en: 'Rebus Foundation Office' }
+    },
+    actor: {
+      type: 'Person',
+      id: 'https://reader-api.test/reader-fmDoik4sRHpLgwJuQHbGVb'
+    },
+    summaryMap: { en: 'someone arrived at Rebus Foundation Office' }
+  },
+  readerId: '7441db0a-c14b-4925-a7dc-4b7ff5d0c8cc',
+  documentId: null,
+  publicationId: null,
+  noteId: null,
+  published: '2018-12-19T14:14:38.124Z',
+  updated: '2018-12-19 14:14:38'
 })
+
+const reader = Object.assign(new Reader(), {
+  id: '7441db0a-c14b-4925-a7dc-4b7ff5d0c8cc',
+  json: { name: 'J. Random Reader', userId: 'auth0|foo1545228877880' },
+  userId: 'auth0|foo1545228877880',
+  published: '2018-12-19T14:14:37.965Z',
+  updated: '2018-12-19 14:14:37',
+  outbox: [activity]
+})
+
 const app = express()
 
 const test = async () => {
@@ -56,11 +84,18 @@ const test = async () => {
 
     await tap.equal(res.statusCode, 200)
 
-    // const body = res.body
-    // await tap.type(body, 'object')
-    // await tap.type(body.id, 'string')
-    // await tap.type(body['@context'], 'string')
-    // await tap.type(body.type, 'string')
+    const body = res.body
+    await tap.type(body, 'object')
+    await tap.type(body.id, 'string')
+    await tap.type(body['@context'], 'string')
+    await tap.equal(body.type, 'OrderedCollection')
+    await tap.type(body.summaryMap, 'object')
+    await tap.ok(Array.isArray(body.orderedItems))
+    await tap.type(body.orderedItems[0], 'object')
+    await tap.type(body.orderedItems[0].type, 'string')
+    await tap.type(body.orderedItems[0].actor, 'object')
+    await tap.type(body.orderedItems[0].summaryMap, 'object')
+    await tap.type(body.orderedItems[0].id, 'string')
   })
 
   // TODO: figure out if 404 error should come from the model throwing an error, or returning undefined
