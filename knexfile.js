198e994b9a11c422515c948238c1c112999aa06f
Baldur Bjarnason <baldur@rebus.foundation>
2019-03-04 13:48:51 -0500

    Updating knexfile.js
    This makes knex.js find the migrations folder based on the relative path from the current module directory instead of the current working directory.
    
    This makes the whole app more reusable as a module

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,17 @@
 // Update with your config settings.
 
+const path = require('path')
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
       filename: process.env.SQLITE_DB || './dev.sqlite3'
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.join(__dirname, 'migrations')
+    }
   },
 
   test: {
@@ -29,7 +34,8 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: path.join(__dirname, 'migrations')
     }
   }
 }
