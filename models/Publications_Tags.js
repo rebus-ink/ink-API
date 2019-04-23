78dce6c9327c35735ac5f1875cbc600a04689478
Marie Pelletier <marie.pelletier@gmail.com>
2019-04-23 13:34:15 -0400

    updated pub-tag model (#235)

--- models/Publications_Tags.js
+++ models/Publications_Tags.js
@@ -1,9 +1,9 @@
 const { Model } = require('objection')
 const { urlToId } = require('../routes/utils')
 
-class Publications_Tags extends Model {
+class Publication_Tag extends Model {
   static get tableName () {
-    return 'publications_tags'
+    return 'publication_tag'
   }
 
   static get idColumn () {
@@ -11,12 +11,11 @@ class Publications_Tags extends Model {
   }
 
   static async addTagToPub (
-    publicationUrl /*: string */,
+    publicationId /*: string */,
     tagId /*: number */
   ) /*: any */ {
     // check publication
-    if (!publicationUrl) return new Error('no publication')
-    const publicationId = await urlToId(publicationUrl)
+    if (!publicationId) return new Error('no publication')
 
     // check tag
     if (!tagId) return new Error('no tag')
@@ -30,32 +29,30 @@ class Publications_Tags extends Model {
     //   return new Error('duplicate')
 
     try {
-      return await Publications_Tags.query().insert({
+      return await Publication_Tag.query().insert({
         publicationId: publicationId,
         tagId
       })
     } catch (err) {
-      if (err.constraint === 'publications_tags_tagid_foreign') {
+      if (err.constraint === 'publication_tag_tagid_foreign') {
         return new Error('no tag')
-      } else if (err.constraint === 'publications_tags_publicationid_foreign') {
+      } else if (err.constraint === 'publication_tag_publicationid_foreign') {
         return new Error('no publication')
       }
     }
   }
 
   static async removeTagFromPub (
-    publicationUrl /*: string */,
+    publicationId /*: string */,
     tagId /*: string */
   ) /*: number */ {
     // check publication
-    if (!publicationUrl) return new Error('no publication')
-
-    const publicationId = await urlToId(publicationUrl)
+    if (!publicationId) return new Error('no publication')
 
     // check tag
     if (!tagId) return new Error('no tag')
 
-    const result = await Publications_Tags.query()
+    const result = await Publication_Tag.query()
       .delete()
       .where({
         publicationId: publicationId,
@@ -70,4 +67,4 @@ class Publications_Tags extends Model {
   }
 }
 
-module.exports = { Publications_Tags }
+module.exports = { Publication_Tag }
