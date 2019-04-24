d80720ebb544809d7df56bf1a38d804990075df2
Sonya <sonya@Sonyas-MacBook-Air.local>
2019-04-24 11:28:22 -0400

    Create note_tag model

---
+++ models/Note_Tag.js
@@ -0,0 +1,70 @@
+const { Model } = require('objection')
+const { urlToId } = require('../routes/utils')
+
+class Note_Tag extends Model {
+  static get tableName () {
+    return 'note_tag'
+  }
+
+  static get idColumn () {
+    return ['noteId', 'tagId']
+  }
+
+  static async addTagToPub (
+    publicationId /*: string */,
+    tagId /*: number */
+  ) /*: any */ {
+    // check publication
+    if (!publicationId) return new Error('no publication')
+
+    // check tag
+    if (!tagId) return new Error('no tag')
+
+    // // check if already exists - SKIPPED FOR NOW
+    // const result = await Publications_Tags.query().where({
+    //   publicationId: publication.id,
+    //   tagId
+    // })
+    // if (result.length > 0) {
+    //   return new Error('duplicate')
+
+    try {
+      return await Publication_Tag.query().insert({
+        publicationId: publicationId,
+        tagId
+      })
+    } catch (err) {
+      if (err.constraint === 'publication_tag_tagid_foreign') {
+        return new Error('no tag')
+      } else if (err.constraint === 'publication_tag_publicationid_foreign') {
+        return new Error('no publication')
+      }
+    }
+  }
+
+  static async removeTagFromPub (
+    publicationId /*: string */,
+    tagId /*: string */
+  ) /*: number */ {
+    // check publication
+    if (!publicationId) return new Error('no publication')
+
+    // check tag
+    if (!tagId) return new Error('no tag')
+
+    const result = await Publication_Tag.query()
+      .delete()
+      .where({
+        publicationId: publicationId,
+        tagId
+      })
+
+    if (result === 0) {
+      return new Error('not found')
+    } else {
+      return result
+    }
+  }
+}
+
+module.exports = { Note_Tag }
