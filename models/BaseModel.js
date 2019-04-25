f58d8da2f16a5c32f500b79b3f39d48066049a4b
Marie Pelletier <marie.pelletier@gmail.com>
2019-04-25 14:21:00 -0400

    fixed broken changes (#240)

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -12,6 +12,7 @@ const URL = require('url').URL
 const debug = require('debug')('hobb:model:base-model')
 const _ = require('lodash')
 const { urlToId } = require('../routes/utils')
+const { createId } = require('./utils')
 
 const domain = process.env.DOMAIN || ''
 
@@ -48,6 +49,10 @@ class BaseModel extends Model {
       json.noteId = `${domain}/note-${json.noteId}`
     }
 
+    if (json.tagId && !json.tagId.startsWith(process.env.DOMAIN)) {
+      json.tagId = `{domain}/tag-${json.tagId}`
+    }
+
     return json
   }
 
@@ -71,10 +76,11 @@ class BaseModel extends Model {
     const parent = super.$beforeUpdate(queryOptions, context)
     let doc = this
     return Promise.resolve(parent).then(function () {
-      doc.id = urlToId(doc.id)
+      // doc.id = createId()
       doc.readerId = urlToId(doc.readerId)
       doc.publicationId = urlToId(doc.publicationId)
       doc.documentId = urlToId(doc.documentId)
+      doc.tagId = urlToId(doc.tagId)
     })
   }
 
@@ -98,170 +104,6 @@ class BaseModel extends Model {
       return undefined
     }
   }
-
-  //   static get jsonAttributes () /*: Array<string> */ {
-  //     return ['json', 'properties']
-  //   }
-  //   get shortId () /*: string */ {
-  //     return translator.fromUUID(this.id)
-  //   }
-  //   hasName () /*: string */ {
-  //     return (
-  //       this.json.name ||
-  //       this.json.nameMap ||
-  //       this.json.summary ||
-  //       this.json.summaryMap
-  //     )
-  //   }
-  //   summarize () /*: string */ {
-  //     let type /*: string */
-  //     if (_.isString(this.type)) {
-  //       type = this.type.toLowerCase()
-  //     } else if (_.isString(this.json.type)) {
-  //       type = this.json.type.toLowerCase()
-  //     } else {
-  //       type = 'object'
-  //     }
-  //     return `${type} with id ${this.id}`
-  //   }
-  //   $beforeUpdate (
-  //     queryOptions /*: any */,
-  //     context /*: any */
-  //   ) /*: Promise<any> */ {
-  //     const parent = super.$beforeUpdate(queryOptions, context)
-  //     const doc = this
-  //     return Promise.resolve(parent).then(function () {
-  //       doc.updated = new Date().toISOString()
-  //     })
-  //   }
-  //   // $parseJson (json /*: any */, opt /*: any */) /*: any */ {
-  //   //   json = super.$parseJson(json, opt)
-  //   //   // Need to discover id, readerId, canonicalId
-  //   //   const id = getUUID(json.id)
-  //   //   let readerId
-  //   //   if (json.bto) {
-  //   //     readerId = getUUID(json.bto)
-  //   //   } else {
-  //   //     readerId = getUUID(json.actor)
-  //   //     json.bto = json.actor
-  //   //   }
-  //   //   const canonicalId = getCanonical(json.url)
-  //   //   const { userId, inReplyTo, context, deleted } = json
-  //   //   // Should get the inReplyTo document id, much like context
-  //   //   const publicationId = getUUID(context)
-  //   //   const documentId = getUUID(inReplyTo)
-  //   //   const activityRelation = objectToId(json.object)
-  //   //   const { attachment, outbox, tag, replies, attributedTo } = addReaderToGraph(
-  //   //     json
-  //   //   )
-  //   //   delete json.bto
-  //   //   delete json.attachment
-  //   //   delete json.replies
-  //   //   delete json.attributedTo
-  //   //   return stripUndefined(
-  //   //     Object.assign(
-  //   //       {
-  //   //         id,
-  //   //         readerId,
-  //   //         canonicalId,
-  //   //         userId,
-  //   //         json,
-  //   //         attachment,
-  //   //         outbox,
-  //   //         tag,
-  //   //         replies,
-  //   //         attributedTo,
-  //   //         publicationId,
-  //   //         documentId,
-  //   //         deleted
-  //   //       },
-  //   //       activityRelation
-  //   //     )
-  //   //   )
-  //   // }
-  //   $formatJson (json /*: any */) {
-  //     const original = super.$formatJson(json)
-  //     json = original.json || {}
-  //     const {
-  //       url: id,
-  //       published,
-  //       updated,
-  //       attachment,
-  //       tags,
-  //       context = {},
-  //       attributedTo = []
-  //     } = original
-  //     json.context = context.id
-  //     return Object.assign(json, {
-  //       id,
-  //       published,
-  //       updated,
-  //       tags,
-  //       attachment,
-  //       attributedTo
-  //     })
-  //   }
-  // }
-  // function getUUID (prop /*: string */) /*: ?string */ {
-  //   try {
-  //     const id = getIdURL(prop)
-  //     const pathname = new URL(id).pathname
-  //     const shortId = pathname.split('-')[1]
-  //     return translator.toUUID(shortId)
-  //   } catch (err) {
-  //     /* should we throw? */
-  //   }
-  // }
-  // function getCanonical (urls /*: Array<string> */ = []) /*: Array<string> */ {
-  //   urls = arrify(urls)
-  //   const link = urls.filter(item => item.rel === 'canonical')
-  //   return lodash.get(link, '0.href')
-  // }
-  // function stripUndefined (json /*: any */) /*: any */ {
-  //   return lodash.omitBy(json, lodash.isUndefined)
-  // }
-  // function getIdURL (idOrObject /*: any */) {
-  //   // not able to type the return value, but it will be a string
-  //   if (lodash.isObject(idOrObject)) {
-  //     return idOrObject.id
-  //   } else {
-  //     return idOrObject
-  //   }
-  // }
-  // function objectToId (obj /*: any */) /*: any */ {
-  //   const id = getUUID(obj)
-  //   const url = getIdURL(obj)
-  //   let type /*: string */
-  //   try {
-  //     const pathname = url.replace(process.env.DOMAIN, '')
-  //     type = pathname.split('-')[0].slice(1)
-  //   } catch (err) {
-  //     return undefined
-  //   }
-  //   return { [type + 'Id']: id }
-  // }
-  // function addReaderToGraph (
-  //   json /*: any */
-  // ) /*: {
-  //   attachment: any,
-  //   outbox: any,
-  //   tag: any,
-  //   replies: any,
-  //   attributedTo: any
-  // } */ {
-  //   const bto = json.bto
-  //   const props = ['attachment', 'outbox', 'tag', 'replies', 'attributedTo']
-  //   const result = {}
-  //   props.forEach(prop => {
-  //     if (json[prop]) {
-  //       debug(prop)
-  //       result[prop] = json[prop].map(item => {
-  //         item.bto = bto
-  //         return item
-  //       })
-  //     }
-  //   })
-  //   return result
 }
 
 module.exports = {
