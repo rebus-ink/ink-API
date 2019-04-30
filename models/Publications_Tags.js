deb8534f27b0daf6258b09cbb24ca3c8ce3e9b1b
Sonya <sonya@Sonyas-MacBook-Air.local>
2019-04-30 09:59:06 -0400

    Make corrections

--- models/Publications_Tags.js
+++ models/Publications_Tags.js
@@ -22,8 +22,6 @@ class Publication_Tag extends BaseModel {
     // check tag
     if (!tagId) return new Error('no tag')
 
-    const newTag = await Tag.byId(tagId)
-
     // // check if already exists - SKIPPED FOR NOW
     // const result = await Publications_Tags.query().where({
     //   publicationId: publication.id,
