89f0d71555cee8b74a25f0f823a0f565127999d5
Sonya <sonya@Sonyas-MacBook-Air.local>
2019-05-08 15:00:52 -0400

    Start integration tests for note-tag

--- routes/activities/add.js
+++ routes/activities/add.js
@@ -1,5 +1,7 @@
 const { createActivityObject } = require('./utils')
 const { Publication_Tag } = require('../../models/Publications_Tags')
+const { Publication } = require('../../models/Publication')
+const { Note_Tag } = require('../../models/Note_Tag')
 const { Activity } = require('../../models/Activity')
 // const { urlToId } = require('./utils')
 
@@ -7,42 +9,56 @@ const handleAdd = async (req, res, reader) => {
   const body = req.body
   switch (body.object.type) {
     case 'reader:Stack':
-      const resultStack = await Publication_Tag.addTagToPub(
-        body.target.id,
-        body.object.id
-      )
+      // Determine if the Tag is added to a Publication or a Note
+      let resultStack
+      if (body.target.type === 'publication') {
+        resultStack = await Publication_Tag.addTagToPub(
+          body.target.id,
+          body.object.id
+        )
+      } else if (body.target.type === 'note') {
+        console.log('This is a note')
 
+        resultStack = await Note_Tag.addTagToNote(
+          body.target.id,
+          body.object.id
+        )
+      }
+
+      // Check for errors
       if (resultStack instanceof Error) {
         switch (resultStack.message) {
           case 'duplicate':
             res
               .status(400)
               .send(
-                `duplicate publication: ${
-                  body.target.id
-                } already asssociated with tag ${body.object.id} (${
-                  body.object.name
-                })`
+                `duplicate` +
+                  body.target.type +
+                  `: ${body.target.id} already asssociated with tag ${
+                    body.object.id
+                  } (${body.object.name})`
               )
             break
-
           case 'no publication':
             res
               .status(404)
               .send(`no publication found with id ${body.target.id}`)
             break
-
           case 'no tag':
             res.status(404).send(`no tag found with id ${body.object.id}`)
             break
-
+          case 'no note':
+            res.status(404).send(`no note found with id ${body.target.id}`)
+            break
           default:
             res.status(400).send(`add tag to publication error: ${err.message}`)
             break
         }
         break
       } else if (!resultStack) {
-        res.status(404).send(`add tag to publication error: ${err.message}`)
+        res
+          .status(404)
+          .send(`add tag to publication or to note error: ${err.message}`)
       }
 
       const activityObjStack = createActivityObject(body, resultStack, reader)
