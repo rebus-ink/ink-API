6c4f879209c083e84c00304c98655bf0a3ac23a4
Marie <marie.pelletier@gmail.com>
2019-05-08 16:44:13 -0400

    pagination seems to work

--- tests/integration/library.test.js
+++ tests/integration/library.test.js
@@ -19,26 +19,35 @@ const test = async () => {
   const userCompleteUrl = await createUser(app, token)
   const userUrl = urlparse(userCompleteUrl).path
 
-  const createPublicationRequest = number => {
-    return JSON.stringify({
-      '@context': [
-        'https://www.w3.org/ns/activitystreams',
-        { reader: 'https://rebus.foundation/ns/reader' }
-      ],
-      type: 'Create',
-      object: {
-        type: 'Publication',
-        name: `Publication ${number}`,
-        author: ['John Smith'],
-        editor: 'Jane Doe',
-        description: 'this is a description!!',
-        keywords: 'one, two',
-        links: [{ property: 'value' }],
-        readingOrder: [{ name: 'one' }, { name: 'two' }, { name: 'three' }],
-        resources: [{ property: 'value' }],
-        json: { property: 'value' }
-      }
-    })
+  const createPublication = async number => {
+    return await request(app)
+      .post(`${userUrl}/activity`)
+      .set('Host', 'reader-api.test')
+      .set('Authorization', `Bearer ${token}`)
+      .type(
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      )
+      .send(
+        JSON.stringify({
+          '@context': [
+            'https://www.w3.org/ns/activitystreams',
+            { reader: 'https://rebus.foundation/ns/reader' }
+          ],
+          type: 'Create',
+          object: {
+            type: 'Publication',
+            name: `Publication ${number}`,
+            author: ['John Smith'],
+            editor: 'Jane Doe',
+            description: 'this is a description!!',
+            keywords: 'one, two',
+            links: [{ property: 'value' }],
+            readingOrder: [{ name: 'one' }, { name: 'two' }, { name: 'three' }],
+            resources: [{ property: 'value' }],
+            json: { property: 'value' }
+          }
+        })
+      )
   }
 
   await tap.test('Get empty library', async () => {
@@ -49,6 +58,7 @@ const test = async () => {
       .type(
         'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
       )
+    console.log(res.error)
     await tap.equal(res.status, 200)
 
     const body = res.body
@@ -127,15 +137,8 @@ const test = async () => {
 
   await tap.test('filter library by collection', async () => {
     // add more publications
-    // publication B
-    const pubBres = await request(app)
-      .post(`${userUrl}/activity`)
-      .set('Host', 'reader-api.test')
-      .set('Authorization', `Bearer ${token}`)
-      .type(
-        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
-      )
-      .send(createPublicationRequest(1))
+    // publication 2
+    const pubBres = await createPublication(2)
 
     const pubActivityUrl = pubBres.get('Location')
     const pubActivityObject = await getActivityFromUrl(
@@ -145,15 +148,8 @@ const test = async () => {
     )
     const publication = pubActivityObject.object
 
-    // publication C
-    const pubcres = await request(app)
-      .post(`${userUrl}/activity`)
-      .set('Host', 'reader-api.test')
-      .set('Authorization', `Bearer ${token}`)
-      .type(
-        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
-      )
-      .send(createPublicationRequest(2))
+    // publication 3
+    await createPublication(3)
 
     // create a stack
     const stackRes = await request(app)
@@ -221,7 +217,87 @@ const test = async () => {
     await tap.equal(body.totalItems, 1)
     await tap.ok(Array.isArray(body.items))
     // documents should include:
-    await tap.equal(body.items[0].name, 'Publication 1')
+    await tap.equal(body.items[0].name, 'Publication 2')
+  })
+
+  await tap.test('paginate library', async () => {
+    // add more publications
+    await createPublication(4)
+    await createPublication(5)
+    await createPublication(6)
+    await createPublication(7)
+    await createPublication(8)
+    await createPublication(9)
+    await createPublication(10)
+    await createPublication(11)
+    await createPublication(12)
+    await createPublication(13)
+
+    // get library with pagination
+    const res = await request(app)
+      .get(`${userUrl}/library?limit=10`)
+      .set('Host', 'reader-api.test')
+      .set('Authorization', `Bearer ${token}`)
+      .type(
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      )
+
+    await tap.equal(res.statusCode, 200)
+
+    const body = res.body
+    await tap.type(body, 'object')
+    await tap.equal(body.totalItems, 10)
+    await tap.ok(Array.isArray(body.items))
+    // documents should include:
+    await tap.equal(body.items.length, 10)
+    await tap.equal(body.items[0].name, 'Publication 13')
+    await tap.equal(body.items[9].name, 'Publication 4')
+    await tap.equal(body.page, 1)
+    await tap.equal(body.pageSize, 10)
+
+    // get page 2
+    const res2 = await request(app)
+      .get(`${userUrl}/library?page=2&limit=10`)
+      .set('Host', 'reader-api.test')
+      .set('Authorization', `Bearer ${token}`)
+      .type(
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      )
+
+    await tap.equal(res2.statusCode, 200)
+
+    const body2 = res2.body
+    await tap.type(body2, 'object')
+    await tap.equal(body2.totalItems, 3)
+    await tap.ok(Array.isArray(body2.items))
+    // documents should include:
+    await tap.equal(body2.items.length, 3)
+    await tap.equal(body2.items[0].name, 'Publication 3')
+    await tap.equal(body2.items[2].name, 'Publication A')
+    await tap.equal(body2.page, 2)
+    await tap.equal(body2.pageSize, 10)
+
+    // testing limit
+    const res3 = await request(app)
+      .get(`${userUrl}/library?page=2&limit=0`)
+      .set('Host', 'reader-api.test')
+      .set('Authorization', `Bearer ${token}`)
+      .type(
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      )
+
+    await tap.equal(res3.body.totalItems, 10)
+
+    // testing default
+    const res4 = await request(app)
+      .get(`${userUrl}/library`)
+      .set('Host', 'reader-api.test')
+      .set('Authorization', `Bearer ${token}`)
+      .type(
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      )
+
+    await tap.equal(res4.body.totalItems, 10)
   })
 
   await tap.test(
