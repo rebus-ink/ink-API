1000c3840399ef1d84ef8cfee1ed7f0a3ffe4f68
Marie <marie.pelletier@gmail.com>
2019-08-12 11:36:46 -0400

    fixing for travis env?

--- utils/queue.js
+++ utils/queue.js
@@ -5,49 +5,53 @@ const utils = require('../utils/utils')
 const storage = new Storage()
 require('dotenv').config()
 
-console.log(process.env)
-const elasticSearchQueue = new Queue('elasticsearch', {
-  redis: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
-  }
-})
+let elasticSearchQueue
 
-elasticSearchQueue.process(async (data, done) => {
-  data = data.data
-  const readingFileStream = storage
-    .bucket(data.bucketName)
-    .file(data.fileName)
-    .createReadStream()
-  let buf = ''
-  readingFileStream
-    .on('data', function (d) {
-      buf += d
-    })
-    .on('end', async () => {
-      request.post(
-        `${process.env.ELASTIC_SEARCH_URL}/document/_doc/`,
-        {
-          auth: {
-            username: process.env.ELASTIC_SEARCH_LOGIN,
-            password: process.env.ELASTIC_SEARCH_PASSWORD
+// skipping this in travis in pull requests because it doesn't have access to redis password
+if (process.env.TRAVIS_SECURE_ENV_VARS) {
+  elasticSearchQueue = new Queue('elasticsearch', {
+    redis: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD
+    }
+  })
+
+  elasticSearchQueue.process(async (data, done) => {
+    data = data.data
+    const readingFileStream = storage
+      .bucket(data.bucketName)
+      .file(data.fileName)
+      .createReadStream()
+    let buf = ''
+    readingFileStream
+      .on('data', function (d) {
+        buf += d
+      })
+      .on('end', async () => {
+        request.post(
+          `${process.env.ELASTIC_SEARCH_URL}/document/_doc/`,
+          {
+            auth: {
+              username: process.env.ELASTIC_SEARCH_LOGIN,
+              password: process.env.ELASTIC_SEARCH_PASSWORD
+            },
+            body: JSON.stringify({
+              name: data.fileName,
+              readerId: utils.urlToId(data.document.readerId),
+              publicationId: data.pubId,
+              documentUrl: data.document.url,
+              documentId: data.document.id,
+              content: buf
+            }),
+            headers: { 'content-type': 'application/json' }
           },
-          body: JSON.stringify({
-            name: data.fileName,
-            readerId: utils.urlToId(data.document.readerId),
-            publicationId: data.pubId,
-            documentUrl: data.document.url,
-            documentId: data.document.id,
-            content: buf
-          }),
-          headers: { 'content-type': 'application/json' }
-        },
-        () => {
-          done()
-        }
-      )
-    })
-})
+          () => {
+            done()
+          }
+        )
+      })
+  })
+}
 
 module.exports = elasticSearchQueue
