b2147d6deda8fcb4c537ae078be5af56e88f0b7f
Marie <marie.pelletier@gmail.com>
2019-10-31 14:53:32 -0400

    fixing conditional tests

--- utils/cache.js
+++ utils/cache.js
@@ -1,32 +1,38 @@
 const redis = require('redis')
-let client
+const { urlToId } = require('./utils')
+
+let libraryCacheGet, libraryCacheUpdate, quitCache
+
 if (process.env.REDIS_PASSWORD) {
-  client = redis.createClient({
+  const client = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD
   })
-}
 
-const { promisify } = require('util')
-const getAsync = promisify(client.get).bind(client)
-const setASync = promisify(client.set).bind(client)
+  const { promisify } = require('util')
+  const getAsync = promisify(client.get).bind(client)
+  const setASync = promisify(client.set).bind(client)
 
-const { urlToId } = require('./utils')
+  libraryCacheUpdate = async readerId => {
+    readerId = urlToId(readerId)
+    return await setASync(
+      `${readerId}-library`,
+      new Date().getTime(),
+      'EX',
+      3600
+    )
+  }
 
-const libraryCacheUpdate = async readerId => {
-  readerId = urlToId(readerId)
-  return await setASync(`${readerId}-library`, new Date().getTime(), 'EX', 3600)
-}
+  libraryCacheGet = async (readerId, check) => {
+    if (!check) return undefined // so we can skip when the 'if-modified-since' header is not used'
+    readerId = urlToId(readerId)
+    return await getAsync(`${readerId}-library`)
+  }
 
-const libraryCacheGet = async (readerId, check) => {
-  if (!check) return undefined // so we can skip when the 'if-modified-since' header is not used'
-  readerId = urlToId(readerId)
-  return await getAsync(`${readerId}-library`)
-}
-
-const quitCache = () => {
-  client.quit()
+  quitCache = () => {
+    client.quit()
+  }
 }
 
 module.exports = { libraryCacheUpdate, libraryCacheGet, quitCache }
