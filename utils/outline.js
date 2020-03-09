580fe0af4961c02cdd01e23ae92546eb64b4afc6
Marie Pelletier <marie.pelletier@gmail.com>
2020-03-09 11:00:16 -0400

    algorithm to convert list to tree

---
+++ utils/outline.js
@@ -0,0 +1,44 @@
+const { arrayToTree } = require('performant-array-to-tree')
+const _ = require('lodash')
+
+const orderLinkedList = list => {
+  const orderedList = []
+
+  const firstOfLists = list.filter(item => !item.previous)
+  if (firstOfLists.length === 0) {
+    throw new Error('Linked list does not have a first item')
+  }
+  firstOfLists.forEach(first => {
+    orderedList.push(first)
+    let current = first
+    while (current.next) {
+      let next = _.find(list, { shortId: current.next })
+      if (!next) {
+        throw new Error(`cannot find 'next' item with id: ${current.next}`)
+      }
+      orderedList.push(next)
+      if (orderedList.length > list.length) {
+        throw new Error('circular linked list')
+      }
+      current = next
+    }
+  })
+
+  return orderedList
+}
+
+const notesListToTree = notes => {
+  const orderedList = orderLinkedList(notes)
+  if (orderedList.length !== notes.length) {
+    throw new Error('Invalid linked list')
+  }
+  const tree = arrayToTree(orderedList, {
+    id: 'shortId',
+    dataField: null,
+    throwIfOrphans: true
+  })
+
+  return tree
+}
+
+module.exports = { notesListToTree }
